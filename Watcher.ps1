ac1d59ea554abffcae00b930ed7c586972f9bc38
Steven Andrews <stevenandrews89@gmail.com>
2019-09-12 17:54:33 +1000

    Seperate watcher and script, Exchange functions working

--- Watcher.ps1
+++ Watcher.ps1
@@ -1,13 +1,3 @@
-## Watcher seteup, checks if file is created and runs script
-$watcher = New-Object System.IO.FileSystemWatcher
-$watcher.Path = "\\SYDMON2016P01\CreateUserAD"
-$watcher.Filter = "*.*"
-$watcher.IncludeSubdirectories = $true
-$watcher.EnableRaisingEvents = $true 
-$action = 
-{
-
-####################################################################################TO DO PERMISSIONS
 ## Let's create a log so we can see what is happening
 Function LogWrite
 {
@@ -24,25 +14,16 @@ Function LogWrite
    Write-Host $Line
 }
 
-$Logfile = "C:\AutoCreateADUser"
-LogWrite "Succesfully imported current block list..."
-####################################################################################TO DO PERMISSIONS
+$Logfile = "C:\AutoCreateADUser\Log.txt"
+LogWrite "Auto Create User Log Started"
 
 ## Define credentials and login to SharePoint
-$Username = “windowsservice@ssw.com.au”
+$Username = “stevenandrews@ssw.com.au”
 $PasswordContent = cat "C:\AutoCreateADUser\PasswordSharePoint.txt"
 $Password = ConvertTo-SecureString -String $PasswordContent -AsPlainText -Force
 $Cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $Username, $Password
 $SiteUrl = "https://sswcom.sharepoint.com/sysadmin"
 Connect-PnPOnline –Url $SiteUrl –Credentials $Cred -NoTelemetry
-
-## Define credentials for Exchange Remote Mailbox Enable
-$Exchange = "SYDEXCH2016P01"
-$Username2 = “SRV_CreateADUser@ssw.com.au”
-$PasswordContent2 = cat "C:\AutoCreateADUser\PasswordExchange.txt"
-$Password2 = ConvertTo-SecureString -String $PasswordContent2 -AsPlainText -Force
-$Cred2 = new-object -typename System.Management.Automation.PSCredential -argumentlist $Username2, $Password2
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$Exchange/PowerShell/" -Authentication Kerberos -Credential $UserCredential
 
 ## Defines List name in SharePoint
 $ListName = "New AD User"
@@ -54,48 +35,52 @@ foreach($User in $UserList)
 {  
     if ($User["SysAdmin_User_Created"] -eq $false)
     {
-        Write-Host "Creating User in AD " $User["SysAdmin_User_GivenName"] " " $User["SysAdmin_User_Surname"]")"
-        
+        Write-Host "Creating User in AD: " $User["SysAdmin_User_GivenName"] " " $User["SysAdmin_User_Surname"]""
         New-ADUser -Name $User["SysAdmin_User_Name"] -GivenName $User["SysAdmin_User_GivenName"] -Surname $User["SysAdmin_User_Surname"] -Description $User["SysAdmin_User_JobTitle"] -DisplayName $User["SysAdmin_User_DisplayName"] -Office $User["SysAdmin_User_Office"] -EmailAddress $User["SysAdmin_User_Email"] -StreetAddress $User["SysAdmin_User_Street"] -City $User["SysAdmin_User_City"] -State $User["SysAdmin_User_State"] -PostalCode $User["SysAdmin_User_PostCode"] -MobilePhone $User["SysAdmin_User_MobilePhone"] -Title $User["SysAdmin_User_JobTitle"] -Manager $User["SysAdmin_User_Manager"] -SamAccountName $User["SysAdmin_User_SAM"] -Path $User["SysAdmin_User_OU"]  -UserPrincipalName $User["Title"] -Company "SSW" -Country $User["SysAdmin_User_Country"]
-
         Set-ADAccountPassword -Identity $User["SysAdmin_User_SAM"] -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "This!c03f0l9" -Force)
-
         Set-AdUser -Identity $User["SysAdmin_User_SAM"] -Enabled:$True -PasswordNeverExpires:$True -ChangePasswordAtLogon:$False
-
         Set-ADUser -Identity $User["SysAdmin_User_SAM"] -Add @{Proxyaddresses="SMTP:"+$User["SysAdmin_User_SAM"]+'@sswcom.onmicrosoft.com';c=$User["SysAdmin_User_Country"]}
-
         $GroupArray = ($User["SysAdmin_User_Groups"] -split ', ')
-
-        foreach($Group in $GroupArray)
-        {  
-            $GroupString = $Group.Replace('"', '') ## Getting rid of quotes around
-
-            Add-ADPrincipalGroupMembership $User["SysAdmin_User_SAM"] -MemberOf $GroupString  
-        } 
-
-        ## Hey Send Email !
+        Add-ADPrincipalGroupMembership $User["SysAdmin_User_SAM"] -MemberOf $GroupString  
+        Write-Host "Finished creating AD user: " $User["SysAdmin_User_GivenName"] " " $User["SysAdmin_User_Surname"]""
+         ## Hey Send Email !
         #TODO: SEND EMAIL Success or Fail could use TRY commads
     }
 }  
 
 ## Porvision Skype Account
+Write-Host "Syncing new users to O365 using AAD Connect"
 Enable-PSRemoting -Force
 Invoke-Command -ComputerName SYDADFSP01 -ScriptBlock { Start-ADSyncSyncCycle -PolicyType Delta }
- Write-Host "Syncing new users to O365 using AAD Connect."
+Write-Host "Syncing complete"
 
 ## Wait for user to be provisioned in O365
-Start-Sleep -Seconds 60
+
+Start-Sleep -Seconds 90
+
+## Define credentials for Exchange Remote Mailbox Enable
+$Exchange = "SYDEXCH2016P01"
+$Username2 = “SRV_CreateADUser@ssw.com.au”
+$PasswordContent2 = cat "C:\AutoCreateADUser\PasswordExchange.txt"
+$Password2 = ConvertTo-SecureString -String $PasswordContent2 -AsPlainText -Force
+$Cred2 = new-object -typename System.Management.Automation.PSCredential -argumentlist $Username2, $Password2
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "http://$Exchange/PowerShell/" -Authentication Kerberos -Credential $Cred2
 
 foreach($User in $UserList)
 {  
+    
+    if ($User["SysAdmin_User_Created"] -eq $false)
+    {
 
-    Write-Host "Enabling Remote Mailbox for " $User["SysAdmin_User_GivenName"] " " $User["SysAdmin_User_Surname"]")"
-    Import-PSSession $Session
-    Enable-RemoteMailbox -Identity $User["SysAdmin_User_SAM"]+@ssw.com.au
+            Write-Host "Enabling Remote Mailbox for " $User["SysAdmin_User_GivenName"] " " $User["SysAdmin_User_Surname"]""
+            Set-ExecutionPolicy RemoteSigned -force
+            Import-PSSession $Session
+            $User=$User["SysAdmin_User_SAM"]
+            Enable-RemoteMailbox -Identity $User@ssw.com.au -RemoteRoutingAddress $User@sswcom.onmicrosoft.com
+            Update-OfflineAddressBook -Identity "Default Offline Address Book"
+            Update-GlobalAddressList -Identity "Default Global Address List"
+            Write-Host "Completed enabling remote mailbox"
 
-}   
+    }   
 
-## Defines what events are being watched
-Register-ObjectEvent $watcher "Created" -Action $action
-
-while ($true) {sleep 5}
+}
